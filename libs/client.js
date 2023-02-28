1a1b7aec6278c4e31bec5598cb5b31cd4baea0be
takumi0512 <tconadora4869@gmail.com>
2023-02-28 18:39:57 +0900

    apikeyの設定変更

--- libs/client.js
+++ libs/client.js
@@ -2,7 +2,9 @@ import { createClient } from "microcms-js-sdk";
 
 export const client = createClient({
     serviceDomain: "blog-cms-t",
-    apiKey: process.env.API_KEY,
+    // 本当は隠す、githubにはapikeyを共有してはいけない
+    // apiKey: process.env.API_KEY,
+    apiKey: LEnTvtpjzh9eDsq2K3U7VjuLYeiRGOh7zPkA
 });
 
 
